bdacfc90d2d0bf461655bdf9a9de0bdfa2217708
silvadealmeida <igotode@gmail.com>
2024-08-22 10:31:49 +0200

    .

--- geonode/base/migrations/0090_auto_20230330_0718.py
+++ geonode/base/migrations/0090_auto_20230330_0718.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("base", "0089_alter_resourcebase_use_constraints"),
+        ("base", "0089_alter_resourcebase_use_constrains"),
     ]
 
     operations = [
