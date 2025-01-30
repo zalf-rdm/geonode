e764e2555e44082b140d1a0adcec5a6723128d63
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2025-01-30 09:23:17 +0100

    [Fixes #222] Bug Patch Map Metadata through v2/api/maps not working

--- geonode/maps/api/views.py
+++ geonode/maps/api/views.py
@@ -161,16 +161,6 @@ class MapViewSet(ApiPresetsInitializer, DynamicModelViewSet, AdvertisedListMixin
                 subtype="tabular-collection" if tabular_collection else None,
             )
         instance = serializer.update(instance,serializer.validated_data)
-        # for key, value in serializer.validated_data.items():
-        #     if hasattr(instance, key):
-        #       try:
-        #           instance.__setattr__(key, value)
-        #       except ValueError:
-        #           pass
-        #     else:
-        #         raise KeyError(key)
-        # instance.save()
-        # return instance
 
         # thumbnail, events and resouce routines
         self._post_change_routines(
