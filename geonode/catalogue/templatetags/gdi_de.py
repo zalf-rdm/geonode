0c3395d72f0ca6cf5c5df2705dbfc83b4c1e1f90
Henning Bredel <h.bredel@52north.org>
2025-10-08 17:23:18 +0200

    Apply Thünen adjustments for GDI-DE compliancy

---
+++ geonode/catalogue/templatetags/gdi_de.py
@@ -0,0 +1,42 @@
+import re
+import requests
+import logging
+
+from django import template
+
+logger = logging.getLogger(__name__)
+
+register = template.Library()
+
+GDI_DE_ROLE_LABEL_MAPPING = {
+    'Owner': 'owner',
+    'Point of Contact': 'pointOfContact',
+    'Metadata Author': 'author',
+    'Processor': 'processor',
+    'Publisher': 'publisher',
+    'Custodian': 'custodian',
+    'Distributor': 'distributor',
+    'Resource User': 'user',
+    'Resource Provider': 'resourceProvider',
+    'Originator': 'originator',
+    'Principal Investigator': 'principalInvestigator'
+}
+
+@register.filter
+def get_gdi_compliant_role_label(value):
+    """
+    Replace labels based on the GDI_DE_ROLE_LABEL_MAPPING dict.
+    Example: {{ role_label_string|get_gdi_compliant_role_label }}
+    """
+    return GDI_DE_ROLE_LABEL_MAPPING.get(value, value)
+
+@register.simple_tag
+def get_individual_name(role):
+    if not role.last_name:
+        return None
+
+    return (
+        f"{role.last_name}, {role.first_name}" 
+        if role.first_name
+        else role.last_name
+    )
