8b535edff0ff02be97a567b158211a9fe696a6db
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2026-07-07 15:17:59 +0200

    [Fixes ]:

--- geonode/settings.py
+++ geonode/settings.py
@@ -2211,7 +2211,7 @@ AVATAR_DELETE_TEMPLATE = "people/avatar/confirm_delete.html"
 
 # disable certain fields from advanded metadata editor
 ADVANCED_EDIT_EXCLUDE_FIELD = [  # base
-    "fundings", # inserted differently, therefore removed here, but still in editor
+    "fundings",  # inserted differently, therefore removed here, but still in editor
     "metadata_only",
     "Region",
     "metadata",
