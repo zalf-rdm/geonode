377fbcd8ab6c3b0507b5dbb73bcccb219e6bc2ff
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2026-07-14 14:40:41 +0200

    [Fixes #632]: "Bug: having `,` inside title gets replaced by `_`"

--- geonode/base/forms.py
+++ geonode/base/forms.py
@@ -678,12 +678,6 @@ class ResourceBaseForm(TranslationModelForm, LinkedResourceForm):
                     _unsescaped_kwds.append(str(_k))
         return _unsescaped_kwds
 
-    def clean_title(self):
-        title = self.cleaned_data.get("title", None)
-        if title:
-            title = title.replace(",", "_")
-        return title
-
     def clean_extra_metadata(self):
         cleaned_data = self.cleaned_data.get("extra_metadata", [])
         return json.dumps(validate_extra_metadata(cleaned_data, self.instance), indent=4)
